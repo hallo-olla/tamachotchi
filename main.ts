1e92bd45c7e3bb7d398ef7488b2a5e45075c4b8f
hallo-olla <124371912+hallo-olla@users.noreply.github.com>
2023-05-12 15:06:13 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let knuffel = 0
+let voeden = 0
+let doden = 0
+basic.forever(function () {
+    knuffel = 20
+    voeden = 20
+    doden = 0
+    if (doden == 0) {
+    	
+    } else {
+    	
+    }
+})
